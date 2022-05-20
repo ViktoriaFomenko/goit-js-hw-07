ecde8b7807e4a44e2ed9f37ac631a2130ef2e97c
ViktoriaFomenko <aktavia@gmail.com>
2022-05-20 12:38:37 +0300

    1 task final before verification

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,14 +1,17 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log(galleryItems);
+console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+let instance;
 
 galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
 
 galleryContainer.addEventListener('click', clickOnGalerryContainer);
+
+document.addEventListener('keydown', escClick);
 
 function createGalleryItemsMarkup(galleryItems) {
   return galleryItems
@@ -28,17 +31,20 @@ function createGalleryItemsMarkup(galleryItems) {
     .join('');
 }
 function clickOnGalerryContainer(event) {
+  console.log(event);
   if (!event.target.classList.contains('gallery__image')) {
     return;
   }
-  const instance = basicLightbox.create(`
+  instance = basicLightbox.create(`
        <img src="${event.target.dataset.source}"
        />
-
   `);
-  instance.show();
 
-  if (event.code === 'Escape') {
+  instance.show();
+}
+
+function escClick(event) {
+  if (event.code === 'Escape' && instance !== undefined) {
     instance.close();
   }
 }
