e321b77ceca5fee745808d31a7fe371d9b0e4eb3
ViktoriaFomenko <aktavia@gmail.com>
2022-05-20 14:02:10 +0300

    task 2 final before verification

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,27 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+                 <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
+                     <img
+                         class="gallery__image"
+                         src="${preview}"
+                         alt="${description}"
+                         title="${description}"
+                     />
+                 </a>
+`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
